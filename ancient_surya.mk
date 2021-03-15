44f3c0269d75e064815b98d70c03d478ca11e93b
kubilhadinata <hadinatasurya@gmail.com>
2021-03-16 00:54:30 +0700

    surya: ancient-ify
    Signed-off-by: kubilhadinata <hadinatasurya@gmail.com>

--- ancient_surya.mk
+++ ancient_surya.mk
@@ -12,14 +12,32 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common WaveOS stuff
+# Inherit some common AncientOS stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-$(call inherit-product, vendor/wave/configs/common.mk)
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+$(call inherit-product, vendor/ancient/configs/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := wave_surya
+PRODUCT_NAME := ancient_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO X3
+PRODUCT_MODEL := POCO X3 NFC
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+#Gapps
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
+
+# Use Background Blur
+TARGET_USES_BLUR := true
+
+# Fingerprint
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ1A.201205.003 7005429 release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
